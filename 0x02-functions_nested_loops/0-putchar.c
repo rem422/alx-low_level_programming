8a9493ce2af30a2cea04576f61d44a651d2251f4
Rem simiyu <remsimiyu@gmail.com>
2022-09-12 13:22:23 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+**/
+
+int main(void)
+{
+int i;
+char j[] = "_putchar";
+
+for (i = 0; i <= 7; i++)
+{
+_putchar(j[i]);
+}
+
+_putchar('\n');
+
+return (0);
+}
