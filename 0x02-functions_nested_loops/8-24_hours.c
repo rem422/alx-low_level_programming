094387605face4ea21bc680a3bf3297ccf40c22d
Rem simiyu <remsimiyu@gmail.com>
2022-09-13 05:24:15 -0700

    'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ void jack_bauer(void)
 int i;
 int j;
 
-for (i = 0; i <= 23; a++)
+for (i = 0; i <= 23; i++)
 {
-for (j = 0; j <= 59; b++)
+for (j = 0; j <= 59; j++)
 {
 _putchar (i / 10 + '0');
 _putchar (i % 10 + '0');
