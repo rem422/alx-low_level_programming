5d9a568a4c096ef2ca07d354ed26b25e4a71d438
Rem simiyu <remsimiyu@gmail.com>
2022-09-13 02:50:48 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+int alphy;
+int count;
+
+count = 0;
+
+while (count < 10)
+{
+for (alphy = 'a'; alphy = 'z'; alphy++)
+{
+_putchar(alphy);
+}
+
+count++;
+_putchar('\n');
+}
+
+}
